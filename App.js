f1df8ff80ad00a40d1bb34024c9dd7cd9909d0cd
samaun37 <76416089+samaun37@users.noreply.github.com>
2021-12-03 06:09:22 +0600

    Add files via upload

---
+++ App.js
@@ -0,0 +1,23 @@
+import { Route, Routes } from 'react-router-dom';
+import AllrecordsPage from './pages/Allrecords';
+import AddnewemployeePage from './pages/Addnew';
+import OthersPage from './pages/Others';
+import MainNavigation from './components/layout/MainNavigation';
+
+function App() {
+  return (
+    <div>
+      <MainNavigation />
+      <Route path='/' exact>
+          <AllrecordsPage/>
+      </Route>
+      <Route path='/add'>
+          <AddnewemployeePage />
+        </Route>
+      <Route path='/other'>
+          <OthersPage />
+      </Route>
+    </div>
+  );
+}
+export default App;
